8d4fcd1fc4df05c2a7d0cc9ba9e23481366638ed
olgabot <olga.botvinnik@gmail.com>
2017-07-04 15:50:06 -0400

    Improve installation experience

--- setup.py
+++ setup.py
@@ -4,13 +4,20 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
+
+def requirements():
+    with open('requirements.txt') as f:
+        return f.read()
+
+
+
 setup(
         name='GPfates',
         version='1.0.0',
         description='Model transcriptional cell fates as mixture of Gaussian Processes',
         long_description=readme(),
         packages=find_packages(),
-        install_requires=['numpy', 'pandas', 'tqdm'],
+        install_requires=requirements(),
         author='Valentine Svensson',
         author_email='valentine@nxn.se',
         license='MIT'
